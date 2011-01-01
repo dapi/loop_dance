82ff5e673b463c314b7c2f42eca4dcfcb77ae114
Danil Pismenny <danil@orionet.ru>
2011-01-01 14:00:35 +0300

    Test rails projects

---
+++ test/rails/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsTest::Application.config.secret_token = '643482964378e906bb239ef558aacddd69c4e3f79dbdb5b249c1f7d4eeed9660d04ff0ae31149b7d9fc44094f85b085b3914045830c719d3f4d134c6ea702ac2'
---
+++ test/rails/lib/dancers.rb
@@ -0,0 +1,18 @@
+class Dancer1 < LoopDance::Dancer
+  every 2.seconds do
+  end
+  every 4.seconds do
+  end
+  every 6.seconds do
+  end
+  every 10.seconds do
+  end
+end
+
+
+class Dancer2 < LoopDance::Dancer
+  every 6.seconds do
+  end
+  every 11.seconds do
+  end
+end
